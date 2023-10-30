08939ebf3abb822c6022972bb9e254b0c408f20c
yashwanth461 <yashwanthkatta31@gmail.com>
2023-10-30 08:25:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from "./Components/Nav";
-import RouterDom from "./Components/RouterDom";
-import Footer from "./Components/Footer";
+import MovieBooking from "./Component/MovieBooking";
+
 function App() {
   return (
     <div>
-      <Nav />
-      <RouterDom />
-      <Footer />
+      <MovieBooking />
     </div>
   );
 }
